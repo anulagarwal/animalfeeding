fbcbf515307ab307e3aed5204300c12cfa52a43b
Anul Agarwal <anulagarwal12@gmail.com>
2021-05-24 19:39:42 +0530

    3rd level done

--- Assets/Scripts/Managers/EnumsManager.cs
+++ Assets/Scripts/Managers/EnumsManager.cs
@@ -2,6 +2,6 @@
 public class EnumsManager 
 {
     public enum State { Idle, Run,Victory};
-    public enum AnimalType { Cat, Dog, Bear, Giraffe};
+    public enum AnimalType { Cat, Dog, Bear, Giraffe, Gorilla};
     public enum FoodType {  Banana, Meat, Cheese, Fish};
 }
